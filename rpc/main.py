1f2ed14ab2f14994dfd0d325b019d1748ec69238
Aspect13 <ad13box@gmail.com>
2022-07-08 15:23:17 +0200

    moving job type api from backend to shared

--- rpc/main.py
+++ rpc/main.py
@@ -1,6 +1,12 @@
+from typing import Optional
+
 from tools import rpc_tools
 from ..models.api_reports import APIReport
 from pylon.core.tools import web
+from pydantic import ValidationError
+
+from ..models.api_tests import PerformanceApiTest
+from ..models.pd.performance_test import TestCommon
 
 
 class RPC:
@@ -8,3 +14,25 @@ class RPC:
     @rpc_tools.wrap_exceptions(RuntimeError)
     def backend_results_or_404(self, run_id):
         return APIReport.query.get_or_404(run_id)
+
+    @web.rpc('backend_test_create_common_parameters', 'parse_common_test_parameters')
+    # @rpc_tools.wrap_exceptions(RuntimeError)
+    @rpc_tools.wrap_exceptions(ValidationError)
+    def parse_common_test_parameters(self, project_id: int, name: str, description: str, **kwargs) -> dict:
+        project = self.context.rpc_manager.call.project_get_or_404(project_id=project_id)
+        pd_object = TestCommon(
+            project_id=project.id,
+            project_name=project.name,
+            name=name,
+            description=description
+        )
+        return pd_object.dict(**kwargs)
+
+    @web.rpc('backend_performance_job_type_by_uid')
+    @rpc_tools.wrap_exceptions(RuntimeError)
+    def job_type_by_uid(self, project_id: int, test_uid: str) -> Optional[str]:
+        test = PerformanceApiTest.query.filter(
+                PerformanceApiTest.get_api_filter(project_id, test_uid)
+        ).first()
+        if test:
+            return test.job_type

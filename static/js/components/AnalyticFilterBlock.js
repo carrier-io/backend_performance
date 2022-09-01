38bbc9b103a16d8313b406b2ffad4809b4078028
Vitalii_Kondratskyi <Vitalii_Kondratskyi@epam.com>
2022-09-02 01:41:39 +0300

    feature(Performance-backend): - highlight chart's line on hover; - added loader for analytic; - refactoring;

--- static/js/components/AnalyticFilterBlock.js
+++ static/js/components/AnalyticFilterBlock.js
@@ -10,6 +10,7 @@ const AnalyticFilterBlock = {
             selectedTransactions: [],
             selectedMetrics: [],
             loaded: false,
+            loadingData: false,
         }
     },
     mounted() {
@@ -36,9 +37,39 @@ const AnalyticFilterBlock = {
                     .filter(item => !filtersBlock.get(this.blockIndex).includes(item.label))
             }
             filtersBlock.set(this.blockIndex, blockItems)
-            this.selectedMetrics.forEach(metric => {
-                getDataForAnalysis(metric, this.selectedTransactions, this.blockIndex);
-            })
+
+            if (this.selectedMetrics.length && this.selectedTransactions.length) {
+                const allRequests = []
+                this.selectedMetrics.forEach(metric => {
+                    allRequests.push(getDataForAnalysis(metric, this.selectedTransactions));
+                })
+                this.fetchChartData(allRequests)
+            }
+        },
+        fetchChartData(allRequests) {
+            this.loadingData = true;
+            Promise.all(allRequests).then(data => {
+                if(analyticsLine.data.labels.length === 0 || analyticsLine.data.labels.length !== data[0].labels.length) {
+                    const firstDatasets = []
+                    data.forEach(item => {
+                        firstDatasets.push(...item.datasets);
+                    })
+                    analyticsData.datasets = firstDatasets;
+                    analyticsData.labels = data[0].labels;
+                    analyticsLine.update();
+                } else {
+                    data.forEach(item => {
+                        analyticsLine.data.datasets.push(...item.datasets);
+                    })
+                    analyticsLine.update();
+                }
+                turnOnAllLine();
+                document.getElementById('chartjs-custom-legend-analytic').innerHTML = analyticsLine.generateLegend();
+            }).catch(error => {
+                console.log('ERROR', error)
+            }).finally(() => {
+                this.loadingData = false;
+            });
         }
     },
     template: `
@@ -54,7 +85,15 @@ const AnalyticFilterBlock = {
                 :items-list="metricItems"
             ></analytic-filter-dropdown>
             <div class="pt-3">
-                <button class="btn btn-secondary" @click="handlerSubmit">Apply</button>
+                <button class="btn btn-secondary"
+                    :disabled="loadingData"
+                    style="position: relative; padding-right: 24px"
+                    @click="handlerSubmit">Apply
+                    <i
+                        v-if="loadingData"
+                        class="spinner-loader" 
+                        style="position: absolute; top: 8px; right: 5px"></i>
+                </button>
             </div>
         </div>
     `

9af7075007e487802de4978512391bc2e4de8a8c
Aspect13 <ad13box@gmail.com>
2022-09-02 20:31:48 +0300

    legend item update

--- static/js/chart_legend.js
+++ static/js/chart_legend.js
@@ -1,5 +1,20 @@
 const LegendItem = {
+    props: [
+        'text', 'borderRadius', 'datasetIndex', 'fillStyle', 'fontColor', 'hidden', 'lineCap', 'lineDash',
+        'lineDashOffset', 'lineJoin', 'lineWidth', 'strokeStyle', 'pointStyle', 'rotation'
+    ],
     template: `
-        
+        <div class="d-flex mb-3">
+            <label class="mb-0 w-100 d-flex align-items-center custom-checkbox custom-checkbox__multicolor">
+                <input type="checkbox" class="mx-2 custom__checkbox" 
+                    checked="true"
+                    id="chart.legend.legendItems[i].datasetIndex"
+                     style="--cbx-color: chart.data.datasets[i].backgroundColor;"  
+                     onclick="updateChart(event, chart.legend.legendItems[i].datasetIndex)"
+                 />
+                 <span class="custom-chart-legend-span"></span>
+                 chart.data.datasets[i].label
+             </label>
+         </div>
     `
 }

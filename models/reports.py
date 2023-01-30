8709a65561a9445387f8ec4eb80ad722cd08e6ff
Aspect13 <ad13box@gmail.com>
2023-01-30 14:36:45 +0400

    adding uuid to test run itself

--- models/reports.py
+++ models/reports.py
@@ -11,6 +11,7 @@
 #     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #     See the License for the specific language governing permissions and
 #     limitations under the License.
+from uuid import uuid4
 
 from sqlalchemy import String, Column, Integer, Float, Text, ARRAY, JSON
 
@@ -74,6 +75,8 @@ class Report(db_tools.AbstractBaseMixin, db.Base):
         return self.serialized.dict(exclude=set(exclude_fields))
 
     def insert(self):
+        if not self.uid:
+            self.uid = str(uuid4())
         if not self.test_config:
             from .tests import Test
             self.test_config = Test.query.filter(

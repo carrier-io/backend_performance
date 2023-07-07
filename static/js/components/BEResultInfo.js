9b4adcc5af54e237db5724fecd90ec5161c4a751
Aspect13 <ad13box@gmail.com>
2023-07-07 14:59:56 +0400

    date formatting fix

--- static/js/components/BEResultInfo.js
+++ static/js/components/BEResultInfo.js
@@ -73,6 +73,10 @@ const BEResultInfo = {
                     this.fetchTags();
                 })
             })
+        },
+        format_date(d) {
+            const date_obj = new Date(d)
+            return isNaN(date_obj) ? '' : date_obj.toLocaleString()
         }
     },
     template: `
@@ -124,11 +128,11 @@ const BEResultInfo = {
                     <table>
                         <tr>
                             <td class="text-gray-500 font-h6 font-semibold font-uppercase pr-3">STARTED</td>
-                            <td class="font-h5" id="start_time">{{ new Date(test_data.start_time)?.toLocaleString() || ''}}</td>
+                            <td class="font-h5" id="start_time">{{ format_date(test_data.start_time) }}</td>
                         </tr>
                         <tr>
                             <td class="text-gray-500 font-h6 font-semibold font-uppercase pr-3">ENDED</td>
-                            <td class="font-h5" id="end_time">{{ new Date(test_data.end_time)?.toLocaleString() || '' }}</td>
+                            <td class="font-h5" id="end_time">{{ format_date(test_data.end_time) }}</td>
                         </tr>
                     </table>
                     <table>
